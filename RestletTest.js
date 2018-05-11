4cbaa75ff145db09bdbdb03c7ba4c44c4a636afc
Leo Galey IV <leog4@KCSF.local>
2018-05-11 14:32:24 -0500

    request/response examples

--- RestletTest.js
+++ RestletTest.js
@@ -1,15 +1,16 @@
 function test() {
-
-    return "Success";
+    var results = nlapiSearchRecord('workorder','customsearch1207');
+    nlapiLogExecution('debug','searchResults',nlapiSearchRecord('workorder','customsearch1207'));
+    return JSON.stringify(results);
 }
 
 
-Authorization:
-OAuth oauth_signature="MgN1gZztYspNQXA576plPD14OWM%3D",
-oauth_version="1.0",
-oauth_nonce="207310548",
-oauth_signature_method="HMAC-SHA1",
-oauth_consumer_key="fvFwnmvurChjol7SZiF2pQ1oJ%2FceRV8vqA%2FrZtzLEo%3D",
-oauth_token="00076e1415667a6c555f5d43582134c87d6367ab456fd2",
-oauth_timestamp="1418647040",
-realm="000068" 
+// Authorization:
+// OAuth oauth_signature="MgN1gZztYspNQXA576plPD14OWM%3D",
+// oauth_version="1.0",
+// oauth_nonce="207310548",
+// oauth_signature_method="HMAC-SHA1",
+// oauth_consumer_key="fvFwnmvurChjol7SZiF2pQ1oJ%2FceRV8vqA%2FrZtzLEo%3D",
+// oauth_token="00076e1415667a6c555f5d43582134c87d6367ab456fd2",
+// oauth_timestamp="1418647040",
+// realm="000068" 
---
+++ requestExample.js
@@ -0,0 +1,5 @@
+GET /app/site/hosting/restlet.nl?script=356&amp;deploy=1 HTTP/1.1
+Host: rest.na2.netsuite.com
+Authorization: OAuth realm="3429264",oauth_consumer_key="82bf5f7084dcff6e790390a3021a5b0fa0dac8c2f8c0cfaf7a30eb26c1236060",oauth_token="c47bd6e5486913b853ed2ceef8654cd6298ea6cbaa3dad254e0342bf1a129ac9",oauth_signature_method="HMAC-SHA1",oauth_timestamp="1526066085",oauth_nonce="MTvmU5",oauth_version="1.0",oauth_signature="04LZMh9fWR0m6IeiTDV63v5l5eg%3D"
+Cache-Control: no-cache
+Postman-Token: 3837fde3-8418-738a-7ed8-bfe4de35ad04
